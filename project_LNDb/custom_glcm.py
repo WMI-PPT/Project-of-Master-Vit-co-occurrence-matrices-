d9c34b55225a27432a909efe7134f47420278134
WMI-PPT <146184915+WMI-PPT@users.noreply.github.com>
2025-06-09 02:13:33 +0800

    Create custom_glcm.py

---
+++ project_LNDb/custom_glcm.py
@@ -0,0 +1,16 @@
+import numpy as np
+from radiomics import glcm
+import logging
+
+class CustomGLCM(glcm.RadiomicsGLCM):
+    def __init__(self, inputImage, inputMask, **kwargs):
+        super().__init__(inputImage, inputMask, **kwargs)
+        self.glcm_raw_matrix = None
+
+    def _calculateMatrix(self, voxelCoordinates=None):
+        result = super()._calculateMatrix(voxelCoordinates=voxelCoordinates)
+        if isinstance(result, np.ndarray):
+            self.glcm_raw_matrix = result.copy()
+        else:
+            logging.warning("GLCM matrix calculation failed or returned unexpected type.")
+        return result
